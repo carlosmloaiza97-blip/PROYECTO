a17d54ea4dd8c9ab701398187f58524ecf53f9be
unknown <Carlosmloaiza97@gmail.com>
2026-09-21 22:21:20 -0500

    Mi primer commit oficial

---
+++ JS-script.js/script.js
@@ -0,0 +1,8 @@
+function saludo() {
+    alert("¡Sistema Operativo!\n\nEjecutando funcionalidad de JavaScript para el proyecto de Gestión de Call Center.\n\nAprendiz: Carlos Loaiza\nFicha: 3465955");
+    console.log("El script de Carlos Loaiza se ha ejecutado correctamente.");
+}
+
+window.onload = function() {
+    console.log("Bienvenido al Panel de Control del Call Center - Sistema Listo.");
+}; // Fin del script
